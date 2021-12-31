cdb6d5fcea96c1a548eb02585f08bffb8235d324
hariskurtishi <hariskay95@gmail.com>
2021-12-31 18:55:15 +0100

    git finder reducers

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,34 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
-const GithubContext = createContext()
+const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-export const GithubProvider = ({children}) => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)    
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: true,
+  };
 
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
-        const data = await response.json()
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
 
-        setUsers(data)
-        setLoading(false)
-    }
+    const data = await response.json();
 
-    return <GithubContext.Provider value={{
-        users,
-        loading,
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
-    }}>
-        {children}
+      }}
+    >
+      {children}
     </GithubContext.Provider>
-}
+  );
+};
 
-export default GithubContext
+export default GithubContext;
